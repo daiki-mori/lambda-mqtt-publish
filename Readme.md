de3af4fa68a3c8ab93993ff1475d04b9fd33b4f5
daiki-mori <user.areaz@gmail.com>
2019-01-13 17:36:40 +0900

    first commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# MQTT Publish Program using AWS Lambda
+I study MQTT Publish and Subscribe.
+Now I use AWS Lambda and AWS IoT, and I send the message using MQTT Publish.
+
+# Requirement
+- Python 3.6
+- MQTT Library(paho-mqtt)(https://qiita.com/areaz_/items/d488fb948de090a7b4c8)
+- Certification of AWS IoT(https://qiita.com/areaz_/items/d488fb948de090a7b4c8)
+
+# Change point of lambda_mqtt_publish.py
+
+# Lambda settings
+
---
+++ lambda_mqtt_publish.py
@@ -0,0 +1,87 @@
+"""
+MQTT Publish
+
+Folder Structure
+/
+┗ mqtt_publish.py
+┗ cert/
+  ┗　AmazonRootCA1.Pem
+  ┗ <id>-certificatie.pem.crt
+  ┗ <id>-private.pem.key
+
+Requeired Library
+- paho-mqtt
+"""
+import ssl
+
+from time import sleep
+
+import paho.mqtt.client as mqtt
+
+#####変更が必要な値#####
+# AWS IoT settings
+ENDPOINT = ""
+CERT_ID = ""
+## AWS IoTで利用するトピック名
+TOPIC = ""
+#####変更が必要な値#####
+
+## マネージメントコンソール→AWS IoT→設定→カスタムエンドポイント にあるエンドポイント名をコピー
+HOST = ENDPOINT + ".iot.ap-northeast-1.amazonaws.com"
+## ポート番号は以下、固定
+PORT = 8883  # port
+## AWS IoTで作成した証明書
+ROOT_CA = "./cert/AmazonRootCA1.pem"
+CLIENT_CERT = "./cert/" + CERT_ID + "-certificate.pem.crt"
+PRIVATE_KEY = "./cert/" + CERT_ID + "-private.pem.key"
+
+CLIENT = None
+
+def on_connect(client, userdata, flags, respons_code):
+    """
+    callback on_connect
+    """
+    print("Connected")
+    print("res_code:" + str(respons_code))
+
+def on_message(client, userdata, msg):
+    """
+    callback on_message
+    """
+    print("message")
+
+def publish(mqtt_client):
+    """
+    publish
+    """
+    print("publish")
+    sleep(2)
+    pub_data = "publish_data"
+    print("TOPIC:" + TOPIC)
+    mqtt_client.publish(TOPIC, pub_data, qos=0)
+    print("send publish")
+
+def create_connection():
+    """
+    Create connection
+    """
+    client = mqtt.Client(protocol=mqtt.MQTTv311)
+
+    # callback
+    client.on_connect = on_connect
+    client.on_message = on_message
+    # certifications
+    client.tls_set(ROOT_CA,
+                   certfile=CLIENT_CERT,
+                   keyfile=PRIVATE_KEY,
+                   cert_reqs=ssl.CERT_REQUIRED,
+                   tls_version=ssl.PROTOCOL_TLSv1_2,
+                   ciphers=None)
+    # port, keepalive
+    client.connect(HOST, port=PORT, keepalive=60)
+    return client
+
+#if __name__ == '__main__':
+def main(event, context):
+    client_connection = create_connection()
+    publish(client_connection)
